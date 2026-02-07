62922144a951c4d9e03a16d35bae3efcccd2e8db
Fernando <fernandoventurini9@hotmail.com>
2026-02-07 14:38:58 +0000

    building an api with node #01

---
+++ README.md
@@ -0,0 +1,19 @@
+### INICIANDO PROJETO
+    -> npm init -y
+
+### INSTALANDO EXPRESS
+    -> npm i express
+
+### HTTP METODOS:
+    -> GET (Listar)
+    -> POST (Criar)
+    -> PUT (Editar Varios)
+    -> PATCH (Editar Um)
+    -> DELETE (Deleta)
+
+### RODANDO O SERVIDOR AUTOMATICAMENTE:
+    -> node --watch server.js
+
+### INSTALANDO PRISMA (FAZ A CONEXAO COM BD)
+    -> npm install prisma --save-dev
+
---
+++ server.js
@@ -0,0 +1,30 @@
+// IMPORTANDO EXPRESS:
+import express from 'express';
+
+// CRIANDO A APLICAÇÃO EXPRESS:
+const app = express();
+app.use(express.json()); // HABILITANDO O PARSE DE JSON PARA AS REQUISIÇÕES RECEBIDAS
+
+// DEFININDO A PORTA:
+const PORT = 3000;
+
+// CRIANDO USUARIOS DE EXEMPLO:
+const users = [];
+
+app.post('/users', (req, res) => {
+    //console.log(req.body); // LOGANDO OS DADOS RECEBIDOS DO CLIENTE
+    users.push(req.body); // ADICIONANDO OS DADOS RECEBIDOS AO ARRAY DE USUARIOS
+    res.status(201).json({ message: 'USUÁRIO ADICIONADO COM SUCESSO!' }); // RESPONDENDO COM UMA MENSAGEM DE SUCESSO
+});
+
+// DEFININDO UMA ROTA PARA O ENDPOINT RAIZ:
+app.get('/users', (req, res) => { 
+    res.status(200).json(users); // RESPONDENDO COM O ARRAY DE USUARIOS EM FORMATO JSON
+});
+
+
+// INICIANDO O SERVIDOR NA PORTA 3000:
+app.listen(PORT, () => {
+    console.log(`SERVIDOR RODANDO NA PORTA ${PORT}!`); // LOGANDO UMA MENSAGEM QUANDO O SERVIDOR ESTIVER PRONTO
+});
+
